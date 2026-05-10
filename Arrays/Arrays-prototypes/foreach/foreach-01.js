a4006c17b9aba6725060d839f9389d49d97d7445
avecek <avecek.rajbanshi011@gmail.com>
2026-05-10 14:33:04 +0545

    Practised foreach using filter

--- Arrays/Arrays-prototypes/foreach/foreach-01.js
+++ Arrays/Arrays-prototypes/foreach/foreach-01.js
@@ -1,4 +1,5 @@
 /**The foreach method of array instance execute the provided function for each array element / it return undefned but howver it iterate over data to do side effects
+ * in simple term it just iterate over the data. no logic problem solving
  * syntax: foreach(callbackfn, thsargs)
  * foreach(element,index,array, thisargs)
  */
@@ -18,5 +19,19 @@ const users = [
 
 users.forEach((user)=> {
     console.log(`welcome ${user.name}! you are ${user.age} years old`);
+    
+})
+
+
+const products = [
+    { name: 'Laptop', price: 1200 },
+    { name: 'Phone', price: 800 },
+    { name: 'Tablet', price: 400 }
+];
+
+const expensiveproduct = products.filter((product) => product.price > 500);
+
+expensiveproduct.forEach((product) => {
+    console.log(`The expensive product is ${product.name} costing ${product.price}`);
     
 })
